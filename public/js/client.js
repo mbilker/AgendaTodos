2d47c7b2b5dd0735ecf44c76b6c5899a2b44334b
mbilker <me@mbilker.us>
2013-03-21 21:35:35 -0400

    Added todos code, split the backbone stuff into separate file and logout stuff in another file, changed the location in layout.jade

--- public/js/client.js
+++ public/js/client.js
@@ -1,29 +1,6 @@
 var App;
 
 $(function() {
-  $('#logout').click(function(e) {
-    e.preventDefault();
-    if (confirm('Are you sure you want to log out?')) {
-      var element = $(this),
-          form = $('<form></form>');
-      form
-        .attr({
-          method: 'POST',
-          action: '/sessions'
-        })
-        .hide()
-        .append('<input type="hidden" />')
-        .find('input')
-        .attr({
-          'name': '_method',
-          'value': 'delete'
-        })
-        .end()
-        .appendTo('body')
-        .submit();
-    }
-  });
-
   var Assignment = Backbone.Model.extend({
     defaults: {
       title: "empty assignment...",
