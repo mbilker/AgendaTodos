6d0f7279586f3fd7560d410cd642efe971d26fc7
mbilker <me@mbilker.us>
2013-03-24 19:25:29 -0400

    Migrated database to another plan on mongohq

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   default: {
-    mongodb: 'mongodb://node:1j24561b3b5j3@widmore.mongohq.com:10000/agenda-book'
+    mongodb: 'mongodb://node:1j24561b3b5j3@dharma.mongohq.com:10017/agenda-book'
   },
   production: {
     mongodb: ''
