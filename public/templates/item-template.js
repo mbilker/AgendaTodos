28e32fea0dedd1294f010bdd12d2093bd3adf3dc
Matt Bilker <me@mbilker.us>
2013-05-06 23:13:43 -0400

    Modified the event handling for editing the title of a task along with the template. Moved the script block for the client from the body to head.

--- public/templates/item-template.js
+++ public/templates/item-template.js
@@ -1,8 +1,8 @@
 <div class="view">
   <label class="checkbox">
-    <input class="toggle" type="checkbox" <%= completed ? 'checked="checked"' : '' %> /><%- title %>
+    <input class="toggle" type="checkbox" <%= completed ? 'checked="checked"' : '' %> /><span class="title"><%- title %></span>
   </label>
-  <p>Due: <%= new Date(dueDate).toDateString() %> - Priority: <%- priority %></p>
+  <span class="details">Due: <%= new Date(dueDate).toDateString() %> - Priority: <%- priority %></span>
   <a class="priority priority<%- priority %>"></a>
   <a class="destroy"></a>
 </div>
