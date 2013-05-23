a4232aef5baea8799369b2b89fb02c066d236de5
Matt Bilker <me@mbilker.us>
2013-05-22 23:55:31 -0400

    More npm deploy work

--- config/npm.rb
+++ config/npm.rb
@@ -6,12 +6,14 @@ namespace :npm do
   end
 
   task :node_additional_setup, :roles => :app, :except => { :no_release => true } do
+    print " --> Creating shared node_modules...".yellow
     run "mkdir -p #{shared_path}/node_modules"
+    puts "Done".green
   end
 
   task :install, roles => :app do
-    print " --> Running npm...".yellow
-    run "cd #{release_path} && npm install --production 2>#{shared_path}/npm.log"
+    print " --> Running npm install...".yellow
+    run "cd #{release_path} && npm install --production 2>#{shared_path}/log/npm.log"
     puts "Done.".green
   end
 end
