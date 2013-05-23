00abd4b6f01e06c312b6d3abd36c07a80f05ebba
Matt Bilker <me@mbilker.us>
2013-05-22 23:53:26 -0400

    Updated npm portion

--- config/npm.rb
+++ config/npm.rb
@@ -1,15 +1,23 @@
-namespace :deploy do
+namespace :npm do
  task :refresh_symlink, roles => :app do
     print " --> Symlinking node_modules...".yellow
     run "rm -rf #{current_path}/node_modules && ln -s #{shared_path}/node_modules #{current_path}/node_modules"
     puts "Done.".green
   end
 
-  task :npm_install, roles => :app do
+  task :node_additional_setup, :roles => :app, :except => { :no_release => true } do
+    run "mkdir -p #{shared_path}/node_modules"
+  end
+
+  task :install, roles => :app do
     print " --> Running npm...".yellow
     run "cd #{release_path} && npm install --production 2>#{shared_path}/npm.log"
     puts "Done.".green
   end
 end
 
-after 'deploy:finalize_update', 'deploy:refresh_symlink', 'deploy:npm_install'
+after 'deploy:create_symlink', 'npm:refresh_symlink'
+after 'deploy:setup', 'npm:node_additional_setup'
+
+before 'deploy:start', 'npm:install'
+before 'deploy:restart', 'npm:install'
